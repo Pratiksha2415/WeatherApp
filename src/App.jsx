f51d91ce7f01d34d28a153640e6326341be83d4a
Pratiksha2415 <chakrabortyshubhjeet@gmail.com>
2025-04-11 00:47:25 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-// import Header  from './Components/Header';
 import './Components/style.css';
-// import Mainfile from './Components/Mainfile';
 
 function App() {
   return (
     <>
   
-    
     </>
     
   );
